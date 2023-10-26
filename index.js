33c56a65ae27af6c046a339929124b0a43577f58
ian <ianokipla@gmail.com>
2023-10-26 09:03:47 +0300

    added style.css file

---
+++ index.js
@@ -0,0 +1,4 @@
+let result = document.getElementById("results")
+let searchBtn = document.getElementById("search-btn")
+let url ="https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+let userInp = document.getElementById
