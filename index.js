5b8b3fa0518e2d7c58fb4d889be5fa1bea1d4e9a
ian <ianokipla@gmail.com>
2023-10-26 11:05:59 +0300

    added index.js file

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 let result = document.getElementById("results")
 let searchBtn = document.getElementById("search-btn")
 let url ="https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
-let userInp = document.getElementById
+let userInp = document.getElementById("user-inp").value;
+if (userInp.length ==0) {
+    result.innerHTML =`<h3 class="msg">The input field cannotbe empty</h3>`;
+}
